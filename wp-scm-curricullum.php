03b08ca1c2076389f9c473b36b4d8229535874a2
edinaldohvieira <edinaldohvieira@gmail.com>
2017-10-09 11:49:33 -0300

    novo plugin

---
+++ wp-scm-curricullum.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WP SCM CURRICULLUM
+ * Plugin URI:      https://github.com/edinaldohvieira/wp-scm-curricullum
+ * Description:     Exibe uma listagem, busca e detalhes dos CURRICULLUMs publicados por cada usuário
+ * Author:          Edinaldo H Vieira
+ * Author URI:      https://github.com/edinaldohvieira
+ * Text Domain:     wp-scm-curricullum
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Scm_Curricullum
+ */
